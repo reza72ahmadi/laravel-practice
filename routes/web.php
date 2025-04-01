9064ad81e937ee9a08671f84b20c901d85e1f4c5
reza72ahmadi <reza72.ahmadi@gmail.com>
2025-04-01 10:32:31 +0430

    download added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Response;
 
 
 
@@ -16,9 +17,17 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/category', function () {
 //     return redirect('home')->with('status', 'Reza');
 // });
-Route::get('/' , function () {
-    return response()->json([
-        'age' => 20,
-        'status' => 'enable',
-    ]);
+// Route::get('/' , function () {
+//     return response()->json([
+//         'age' => 20,
+//         'status' => 'enable',
+//     ]);
+// });
+
+Route::get('/download', function () {
+    $pathToFile = public_path('files/sample.pdf'); // Update with your file path
+    $name = 'downloaded_file.pdf'; // Update with desired file name
+    $headers = ['Content-Type: application/pdf']; // Update based on file type
+
+    return Response::download($pathToFile, $name, $headers)->deleteFileAfterSend(true);
 });
